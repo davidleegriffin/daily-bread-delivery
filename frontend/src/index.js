0fb989e6089c750d2689fb91cbe08c932269f771
= <davidleegriffin@yahoo.com>
2021-01-05 19:04:27 -0600

    csrf token est

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { restoreCSRF, fetch } from './store/csrf';
 import './index.css';
 import App from './App';
 import configureStore from './store';
@@ -9,6 +10,9 @@ import configureStore from './store';
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = fetch;
   window.store = store;
 }
 
