e9ae69676416911146f327a81e6971f092941aa6
= <davidleegriffin@yahoo.com>
2021-01-10 16:13:14 -0600

    break time

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,12 +2,34 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import { useHistory } from "react-router-dom";
-
+import Slider from "react-slick";
 import './HomePage.css';
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "inline", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
 
 function HomePage() {
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const settings = {
+    dots: true,
+      lazyLoad: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      initialSlide: 2,
+      nextArrow: <SampleNextArrow />,
+  };
 
   const logout = (e) => {
     // e.preventDefault();
@@ -28,7 +50,7 @@ function HomePage() {
       <div className="home__button-container--logout">
         <button className="logout-button" onClick={logout}>Log Out</button>
       </div>
-      <div>
+      {/* <div>
         <img
           className="home__image--left"
           src="https://images.unsplash.com/photo-1549057188-efd70413345e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJha2VyeXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
@@ -37,7 +59,7 @@ function HomePage() {
           height="120%"
         >
         </img>
-      </div>
+      </div> */}
       <div className="home__image-container--chalkboard">
         <span className="home__chalkboard-text--container">
             <h2>WELCOME TO DAILY BREAD DELIVERY</h2>
@@ -52,7 +74,7 @@ function HomePage() {
           </p>
         </span>
       </div>
-      <div>
+      {/* <div>
         <img
           className="home__image--right"
           src="https://images.unsplash.com/photo-1560427183-4efd29c38997?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fGJha2VyeXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
@@ -61,16 +83,78 @@ function HomePage() {
           height="120%"
         >
         </img>
-      </div>
+      </div> */}
       <div className="home__image-container--bottom">
-        <img
-          className="home__image--bottom"
-          src="https://images.unsplash.com/photo-1549931319-a545dcf3bc73?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTl8OTc0NzE2NTd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          alt="bread"
-          // width="60%"
-         
-        >
-        </img>
+        <Slider {...settings}>
+          <div className="home__fade-elements">
+            <img
+              className="home__image--bottom"
+              src="https://images.unsplash.com/photo-1549931319-a545dcf3bc73?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTl8OTc0NzE2NTd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              alt="bread-1"
+              // width="300px"
+              // height="300px"
+            >
+            </img>
+          </div>
+          <div className="home__fade-elements">
+            <img
+              className="home__image--bottom"
+              src="https://images.unsplash.com/photo-1603984042729-a34adc2ac9d0?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTV8OTc0NzE2NTd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              alt="bread-2"
+            >
+            </img>
+          </div>
+          <div className="home__fade-elements">
+            <img
+              className="home__image--bottom"
+              src="https://images.unsplash.com/photo-1533417177250-227597f5b264?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTh8OTc0NzE2NTd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              alt="bread-3"
+              // width="300px"
+              // height="300px"
+            >
+            </img>
+          </div>
+          <div className="home__fade-elements">
+            <img
+              className="home__image--bottom"
+              src="https://images.unsplash.com/photo-1511629036492-6c07153d3e83?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTZ8OTc0NzE2NTd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              alt="bread-4"
+              // width="300px"
+              // height="300px"
+            >
+            </img>
+          </div>
+          <div className="home__fade-elements">
+            <img
+              className="home__image--bottom"
+              src="https://images.unsplash.com/photo-1590346328376-f21c8e5b630e?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTd8OTc0NzE2NTd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              alt="bread-5"
+              width="300px"
+              height="300px"
+            >
+            </img>
+          </div>
+          <div className="home__fade-elements">
+            <img
+              className="home__image--bottom"
+              src="https://images.unsplash.com/photo-1584471973216-cadfdf0e15e5?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTR8OTc0NzE2NTd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              alt="bread-6"
+              width="300px"
+              height="300px"
+            >
+            </img>
+          </div>
+          <div className="home__fade-elements">
+            <img
+              className="home__image--bottom"
+              src="https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTN8OTc0NzE2NTd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              alt="bread-7"
+              width="300px"
+              height="300px"
+            >
+            </img>
+          </div>
+        </Slider>
       </div>
     </div>
   )
