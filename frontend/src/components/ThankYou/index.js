29e5338a3fed95b1734d330267b6ea0c7031e779
= <davidleegriffin@yahoo.com>
2021-01-27 20:58:21 -0600

    finished final presentation stage beginning

--- frontend/src/components/ThankYou/index.js
+++ frontend/src/components/ThankYou/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 import './ThankYou.css';
 
 function ThankYou() {
   return (
     <div>
       <h1>THANK YOU APP ACADEMY!!!!</h1>
+      <NavLink
+            className="cart__navbar"
+            to={{
+              pathname: "/home",
+              cartProps: {
+                name: "cart"
+              }
+            }}>
+            <button className="checkout__button--home">Home</button>
+        </NavLink>
     </div>
   )
 }
