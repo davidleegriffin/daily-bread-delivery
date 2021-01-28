09c2adb20a31d03ba5fcb44c79f056df3dd3007c
= <davidleegriffin@yahoo.com>
2021-01-28 12:02:28 -0600

    styling thanks page

--- frontend/src/components/ThankYou/index.js
+++ frontend/src/components/ThankYou/index.js
@@ -4,18 +4,24 @@ import './ThankYou.css';
 
 function ThankYou() {
   return (
-    <div>
-      <h1>THANK YOU APP ACADEMY!!!!</h1>
-      <NavLink
-            className="cart__navbar"
-            to={{
-              pathname: "/home",
-              cartProps: {
-                name: "cart"
-              }
-            }}>
-            <button className="checkout__button--home">Home</button>
-        </NavLink>
+    <div className="thanks__container--page">
+      <div className="pyro">
+      <div className="before"></div>
+        <div className="after"></div>
+        <div className="thanks__container--banner">
+          <h1 className="thanks__banner">THANK YOU APP ACADEMY!!!!</h1>
+        </div>
+        <NavLink
+              className="cart__navbar"
+              to={{
+                pathname: "/home",
+                cartProps: {
+                  name: "cart"
+                }
+              }}>
+              <button className="checkout__button--home">Home</button>
+          </NavLink>
+      </div>
     </div>
   )
 }
